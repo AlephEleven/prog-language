5bb608ae7eaf9caedfa6c775a470baac7766e35f
AlephEleven <lambdasauce2@gmail.com>
2022-03-13 18:47:12 -0400

    parser

--- main/parser.py
+++ main/parser.py
@@ -4,7 +4,6 @@ from lexer import Token
 Given a list of tokens, we want to use a set of concrete syntax rules in order to create a
 syntax tree, and finalize it by making an AST
 '''
-
 def concrete_type(token):
     match token:
         case {"NUMBER": _}:
@@ -17,28 +16,48 @@ def concrete_type(token):
 def concrete_list(tk_list):
     return [concrete_type(i) for i in tk_list]
 
-def concrete_defs(conc_list):
-    match conc_list:
-        case []:
+'''
+Concrete Syntax for language, checks for pattern in concrete list, if none found, returns current index (head)
+loops through entire list
+'''
+
+
+def concrete_defs(conc_list, prec=-1):
+    match conc_list, prec:
+        case [], _:
             return []
-        #<Exp> := <EXP> <BOp> <Exp>
-        case [{"EXP": e1}, {"OP": op}, {"EXP": e2}, *t]:
-            return [{"EXP": [{"EXP": e1}, {"OP": op}, {"EXP": e2}]}] + concrete_defs(t)
         #<Exp> := (<Exp>)
-        case [{"LBRAC": lp}, {"EXP": e}, {"RBRAC": rp}, *t]:
-            return [{"EXP": [{"LBRAC": lp}, {"EXP": e}, {"RBRAC": rp}]}] + concrete_defs(t)
+        case [{"LBRAC": lp}, {"EXP": e}, {"RBRAC": rp}, *t], 1:
+            return [{"EXP": [{"LBRAC": lp}, {"EXP": e}, {"RBRAC": rp}]}] + concrete_defs(t, prec)
+        #<Exp> := <Exp> <BOp> <Exp> (with PEMDAS precedence)
+        case [{"EXP": e1}, {"OP": op}, {"EXP": e2}, *t], _ if prec > 1:
+            print(f"{op} \t {prec}")
+            match op, prec:
+                case {"MULT": _} | {"DIV": _}, 2:
+                    return [{"EXP": [{"EXP": e1}, {"OP": op}, {"EXP": e2}]}] + concrete_defs(t, prec)
+                case {"PLUS": _} | {"MINUS": _}, 3:
+                    return [{"EXP": [{"EXP": e1}, {"OP": op}, {"EXP": e2}]}] + concrete_defs(t, prec)
+                case _, _:
+                    return [{"EXP": e1}, {"OP": op}, {"EXP": e2}]+concrete_defs(t, prec)
         #<Any> := <Any>
-        case [h, *t]:
-            return [h]+concrete_defs(t)
+        case [h, *t], _:
+            return [h]+concrete_defs(t, prec)
 
-defs = concrete_list(Token.parse_string("1+1+1+1+1+1+1+1+1+1+1"))
 
+'''
+Generates a Concrete Syntax Tree, loops through concrete definitions, and continously pattern matches
+from left-to-right until there are no changes when given to the defintions twice
+
+Raises exception if unable to make CST (list)
+'''
 def gen_CST(conc_list, alarm=2):
     i = 0
     cst = conc_list
     while(i < alarm):
         tmp_cst = cst
-        cst = concrete_defs(cst)
+        cst = concrete_defs(cst, 1)
+        cst = concrete_defs(cst, 2)
+        cst = concrete_defs(cst, 3)
 
         i = i+1 if tmp_cst==cst else 0
 
@@ -47,6 +66,10 @@ def gen_CST(conc_list, alarm=2):
 
     return cst
 
+tst = "4+(3*(4/2))/2+(1)"
+
+defs = concrete_list(Token.parse_string("4 + 3 * 2 "))
+print(gen_CST(defs))
 
 
 
