43638e524ac8b3c310e091b10dc320df3b46225d
AlephEleven <lambdasauce2@gmail.com>
2022-03-15 06:31:02 -0400

    added ds

---
+++ main/ds.py
@@ -0,0 +1,30 @@
+from abstree import expr_cls
+
+'''
+Abstracted Results
+'''
+
+#evaluates final result
+def result(exp):
+    match exp.id:
+        case "Ok":
+            return exp
+        case "Error":
+            raise Exception(exp.vals)
+
+#Abstracted valid exp type
+def return_exp(exp):
+    return expr_cls("Ok", exp, f"Ok ({exp})")
+
+#Abstracted error type
+def error_exp(s):
+    return expr_cls("Error", s, f'Error "{s}"')
+
+
+#binding function, checks whether to propagate error, or apply function
+def pass_exp(exp, f):
+    match exp.id:
+        case "Error":
+            return exp
+        case "Ok":
+            return f(exp)
--- main/interp.py
+++ main/interp.py
@@ -1,7 +1,20 @@
 from lexer import Token
 from parser import CST
 from abstree import AST
+from main.ds import *
 
+'''
+Here is where we want to finally evaluate expressions that come from the AST
+'''
+
+
+
+
+
+
+'''
+Application functions
+'''
 
 def string_to_CST(s):
     return CST.parse_tokens(Token.parse_string(s))
